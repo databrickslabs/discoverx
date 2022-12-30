964f48b3807479472a5aca67b8424dbf85ab0821
Erni Durdevic <erni.durdevic@databricks.com>
2022-12-30 16:08:10 +0100

    Added multiple rule tests

--- discoverx/sql/analyze_content.py
+++ discoverx/sql/analyze_content.py
@@ -9,11 +9,11 @@ def rule_matching_sql(
     expressions = [r for r in rules if r.type == "regex"]
     cols = [c for c in table_info.columns if c.data_type == "string"]
 
-    matchingColumns = [f"INT(regexp_like(value, '{r.definition}')) AS {r.name}" for r in expressions]
-    matchingString = ",\n        ".join(matchingColumns)
+    matching_columns = [f"INT(regexp_like(value, '{r.definition}')) AS {r.name}" for r in expressions]
+    matching_string = ",\n                ".join(matching_columns)
 
-    unpivotExpressions = ", ".join([f"'{r.name}', {r.name}" for r in expressions])
-    unpivotColumns = ", ".join([f"'{c.name}', {c.name}" for c in cols])
+    unpivot_expressions = ", ".join([f"'{r.name}', {r.name}" for r in expressions])
+    unpivot_columns = ", ".join([f"'{c.name}', {c.name}" for c in cols])
 
     sql = f"""
         SELECT 
@@ -25,15 +25,15 @@ def rule_matching_sql(
             (sum(value) / count(value)) as frequency
         FROM
         (
-            SELECT column, stack({len(expressions)}, {unpivotExpressions}) as (rule_name, value)
+            SELECT column, stack({len(expressions)}, {unpivot_expressions}) as (rule_name, value)
             FROM 
             (
                 SELECT
                 column,
-                {matchingString}
+                {matching_string}
                 FROM (
                 SELECT
-                    stack({len(cols)}, {unpivotColumns}) AS (column, value)
+                    stack({len(cols)}, {unpivot_columns}) AS (column, value)
                 FROM {table_info.database}.{table_info.table}
                 TABLESAMPLE ({sample_size} ROWS)
                 )
--- tests/unit/sql/analyze_content_test.py
+++ tests/unit/sql/analyze_content_test.py
@@ -1,5 +1,6 @@
 from discoverx.sql.analyze_content import *
 from discoverx.data_models import *
+import logging
 
 def test_generate_sql():
 
@@ -37,7 +38,48 @@ FROM
 )"""
     actual = rule_matching_sql(table_info, rules, 100)
 
-    print(actual)
-
+    logging.info(f"Generated SQL is: \n{actual}")
+    
     assert actual == expected
 
+def test_generate_sql_multiple_rules():
+
+    columns = [
+        ColumnInfo("id", "number", False),
+        ColumnInfo("name", "string", False)
+    ]
+    table_info = TableInfo("meta", "db", "tb", columns)
+    rules = [
+        Rule("any_word", "regex", "Any word", "\w."),
+        Rule("any_number", "regex", "Any number", "\d."),
+    ]
+
+    expected = """SELECT
+    'meta' as metastore,
+    'db' as database,
+    `tb` as table,
+    column,
+    rule_name,
+    (sum(value) / count(value)) as frequency
+FROM
+(
+    SELECT column, stack(2, 'any_word', any_word, 'any_number', any_number) as (rule_name, value)
+    FROM
+    (
+        SELECT
+        column,
+        INT(regexp_like(value, '\w.')) AS any_word,
+        INT(regexp_like(value, '\d.')) AS any_number
+        FROM (
+        SELECT
+            stack(1, 'name', name) AS (column, value)
+        FROM db.tb
+        TABLESAMPLE (100 ROWS)
+        )
+    )
+)"""
+    actual = rule_matching_sql(table_info, rules, 100)
+
+    logging.info(f"Generated SQL is: \n{actual}")
+    
+    assert actual == expected
