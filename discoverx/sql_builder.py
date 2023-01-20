81506be4672d896383ebf1c67fa288f36a7364e5
david.tempelmann <david.tempelmann@databricks.com>
2023-01-20 09:30:07 +0100

    Add docstring and fix pylint warnings

--- discoverx/sql_builder.py
+++ discoverx/sql_builder.py
@@ -1,9 +1,36 @@
-from discoverx.config import *
+"""
+This module contains classes and functions which automatically build
+SQL expressions for specified tables and rules
+"""
+from discoverx.config import TableInfo, Rule
 from discoverx.common import trim_sql
 
 
 class SqlBuilder:
+    """
+    The SqlBuilder class automatically creates a SQL expression for
+    scanning tables and determining the likelihood of its columns to
+    match specified rules
+    """
+    # TODO: move table_info, rules, ... to an init method
+    # pylint: disable=too-few-public-methods
     def rule_matching_sql(self, table_info: TableInfo, rules: list[Rule], sample_size: int = 1000):
+        """
+        Given a table and a set of rules this method will return a
+        SQL expression which matches the table's columns against rules.
+        If executed on the table using SQL the output will contain a
+        matching frequency (probability) for each column and rule.
+        Args:
+            table_info (TableInfo): Specifies the table to be scanned
+            rules (list[Rule]): A list of Rules based on regular
+                expressions
+            sample_size (int): The number of records sampled/scanned
+                for each table
+
+        Returns:
+            string: The SQL expression
+
+        """
 
         expressions = [r for r in rules if r.type == "regex"]
         cols = [c for c in table_info.columns if c.data_type == "string"]
