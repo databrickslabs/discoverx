0752ee221d2142f99af59b0aa675b487c22b498f
david.tempelmann <david.tempelmann@databricks.com>
2023-02-22 15:55:07 +0100

    Add docstrings and fix pylint-raised issues

--- discoverx/rules.py
+++ discoverx/rules.py
@@ -1,3 +1,8 @@
+"""
+This module implements rules. Both the structure of rules as well as
+actual built-in rules are defined.
+"""
+
 from dataclasses import dataclass
 from enum import Enum
 from fnmatch import fnmatch
@@ -6,14 +11,16 @@ from pydantic import BaseModel
 
 
 class RuleTypes(str, Enum):
-    regex = "regex"
+    """Allowed types/choices for Rules
+    """
+    REGEX = "regex"
 
 
 class Rule(BaseModel):
+    """Definition of Rules
+    We use pydantic to make sure custom rules defined by users
+    are validated
     """
-    Use pydantic for rules to ensure user input is validated
-    """
-
     name: str
     type: RuleTypes
     description: str
@@ -21,12 +28,20 @@ class Rule(BaseModel):
     example: Optional[str] = None
     tag: Optional[str] = None
 
+
 @dataclass
 class RulesList:
+    """A list of rules with added properties
+    Attributes:
+    -----------
+    rules (List(Rule), optional): A list of rules, could be either
+        built-in or custom-defined. Defaults to None
+    """
     rules: Optional[List[Rule]] = None
 
     @property
     def rules_info(self) -> str:
+        """Get a HTML-formatted description of all the rules"""
         if self.rules:
             text = [f"<li>{rule.name} - {rule.description}</li>" for rule in self.rules]
             return "\n              ".join(text)
@@ -35,6 +50,7 @@ class RulesList:
 
     @property
     def number_of_rules(self) -> int:
+        """Return the number of rules in the list."""
         if self.rules:
             return len(self.rules)
         return 0
@@ -56,12 +72,29 @@ ip_v6_rule = Rule(
 
 
 class Rules:
+    """This class represents built-in and custom rules
+
+    Attributes:
+        builtin_rules (RulesList): Represents all built-in rules
+        custom_rules (RulesList): Represents all custom-defined rules
+
+    """
     def __init__(self, custom_rules: Optional[List[Rule]] = None):
+        """
+        Args:
+            custom_rules (List[Rule], optional): A list of
+                custom-defined rules. Defaults to None.
+        """
         self.builtin_rules: RulesList = RulesList([ip_v4_rule, ip_v6_rule])
         self.custom_rules = RulesList(custom_rules)
 
     def get_rules_info(self):
+        """Returns info about the available rules
+        Both built-in and custom rules will be presented
 
+        Returns:
+            HTML-formatted string
+        """
         text = f"""
                 <h2>Matching rules</h2>
                 <p>
@@ -80,10 +113,37 @@ class Rules:
         return text
 
     def get_rules(self, rule_filter: str) -> List[Rule]:
+        """Return a list of specified rules
+        The rules to be returned are specified using unix wildcards.
+        Both built-in and custom rules are returned in one single list.
+
+        Args:
+            rule_filter (str): Unix wildcard string specifying the
+                rule(s) to be returned. E.g. '*' for all rules while
+                '*v4' would return all rules with names matching the
+                wildcard. This would include the 'ip_v4' built-in rule.
+
+        Returns:
+            A list of rules matching the wildcard
+        """
         filtered_builtin_rules = self._filter_rules(self.builtin_rules.rules, rule_filter)
         filtered_custom_rules = self._filter_rules(self.custom_rules.rules, rule_filter)
         return filtered_builtin_rules + filtered_custom_rules
 
     @staticmethod
     def _filter_rules(rule_set: List[Rule], rule_filter: str) -> List[Rule]:
+        """Filter a given set of rules
+
+        Args:
+            rule_set (List[Rule]): The set of rules to be filtered, e.g.
+                the built-in or custom rules.
+            rule_filter (str): Unix wildcard string specifying the
+                rule(s) to be returned. E.g. '*' for all rules while
+                '*v4' would return all rules with names matching the
+                wildcard. This would include the 'ip_v4' built-in rule.
+
+        Returns:
+            A list of rules from the specified rule set matching the
+            wildcard.
+        """
         return [rule for rule in (rule_set if rule_set is not None else []) if fnmatch(rule.name, rule_filter)]
