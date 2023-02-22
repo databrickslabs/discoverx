ffc1f7327ea58f15fd02634c43b8caa34d0c43be
Erni Durdevic <erni.durdevic@databricks.com>
2023-02-22 13:40:01 +0100

    Added filter tables

--- discoverx/data_model.py
+++ discoverx/data_model.py
@@ -1,5 +1,5 @@
 from discoverx.config import TableInfo
-
+import fnmatch
 class DataModel:
     def __init__(self, logger=None):
         
@@ -8,8 +8,14 @@ class DataModel:
             logger = Logging()
         self.logger = logger
 
+    def _apply_filter(self, item: TableInfo, catalogs_filter, databases_filter, tables_filter):
+        return fnmatch(item.catalog, catalogs_filter) and fnmatch(item.database, databases_filter) and fnmatch(item.table, tables_filter)
+
     def get_table_list(self, catalogs_filter, databases_filter, tables_filter):
-        return [
+        table_info = [
             TableInfo("default", "sklearn_housing", "sklearn_housing", []),
             TableInfo("default", "sklearn_housing", "sklearn_housing_2", []),
         ]
+        filtered_tables = [x for x in table_info if self._apply_filter(x, catalogs_filter, databases_filter, tables_filter)]
+
+        return filtered_tables
