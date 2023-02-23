7d48b3a00fcc46800464ff8e96a31ceb0b92d852
david.tempelmann <david.tempelmann@databricks.com>
2023-02-23 09:00:19 +0100

    add validator for given rule example

--- discoverx/rules.py
+++ discoverx/rules.py
@@ -6,27 +6,48 @@ actual built-in rules are defined.
 from dataclasses import dataclass
 from enum import Enum
 from fnmatch import fnmatch
+import re
 from typing import Optional, List
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class RuleTypes(str, Enum):
-    """Allowed types/choices for Rules
-    """
+    """Allowed types/choices for Rules"""
+
     REGEX = "regex"
 
 
 class Rule(BaseModel):
-    """Definition of Rules
-    We use pydantic to make sure custom rules defined by users
-    are validated
+    """Definition of Rule
+    Attributes:
+        name (str): Name of rule
+        type (RuleTypes): The type of the rule. Currently, only 'regex'
+            is supported
+        description (str): Description of the rule
+        definition (str): The actual definition. I.e. for regex-type
+            rules this would contain the actual regular expressions as
+            a RAW string, i.e. r'...'
+        example (str, optional): It is possible to provide an example of
+            a string/expression to be matched by the Rule. This example
+            is used to validate the Rule upon instantiation.
+        tag (str, optional): Name used to tag matched columns in unity
+            catalog
     """
+
     name: str
     type: RuleTypes
     description: str
     definition: str
     example: Optional[str] = None
     tag: Optional[str] = None
+
+    # pylint: disable=no-self-argument
+    @validator("example")
+    def validate_rule(cls, example, values):
+        """Validates that given example is matched by defined pattern"""
+        if (values["type"] == RuleTypes.REGEX) and not re.match(values["definition"], example):
+            raise ValueError(f"The definition of the rule {values['name']} does not match the provided example")
+        return example
 
 
 @dataclass
@@ -37,6 +58,7 @@ class RulesList:
     rules (List(Rule), optional): A list of rules, could be either
         built-in or custom-defined. Defaults to None
     """
+
     rules: Optional[List[Rule]] = None
 
     @property
@@ -61,13 +83,13 @@ ip_v4_rule = Rule(
     name="ip_v4",
     type="regex",
     description="IP address v4",
-    definition=r"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",
+    definition=r"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",
 )
 ip_v6_rule = Rule(
     name="ip_v6",
     type="regex",
     description="IP address v6",
-    definition=r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))",
+    definition=r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))",
 )
 
 
@@ -79,6 +101,7 @@ class Rules:
         custom_rules (RulesList): Represents all custom-defined rules
 
     """
+
     def __init__(self, custom_rules: Optional[List[Rule]] = None):
         """
         Args:
