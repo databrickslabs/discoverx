4491980282d744217dec35ca4ab644052316a1fb
erni.durdevic@databricks.com <erni.durdevic@databricks.com>
2023-06-20 02:13:21 +0000

    Added stacked query

--- notebooks/interaction_ui.py
+++ notebooks/interaction_ui.py
@@ -20,20 +20,9 @@
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- Clean up old demos
-# MAGIC DROP TABLE IF EXISTS _discoverx.classification.classes;
-
-# COMMAND ----------
-
-# Generate sample data
-dbutils.notebook.run("./sample_data", timeout_seconds=0, arguments={"discoverx_sample_catalog": "discoverx_sample"} )
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC ## DiscoverX Interaction
-# MAGIC 
+# MAGIC
 # MAGIC In the following we demonstrate how to interact with DiscoverX.
 
 # COMMAND ----------
@@ -56,11 +45,19 @@ dx.scan(from_tables="discoverx*.*.*")
 
 # COMMAND ----------
 
+dx.scan_result()
+
+# COMMAND ----------
+
+dx.publish()
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## Search
-# MAGIC 
+# MAGIC
 # MAGIC This command can be used to search inside the content of tables.
-# MAGIC 
+# MAGIC
 # MAGIC If the tables have ben scanned before, the search will restrict the scope to only the columns that could contain the search term based on the avaialble rules.
 
 # COMMAND ----------
@@ -80,7 +77,7 @@ dx.select_by_classes(from_tables="discoverx*.*.*", by_classes=["ip_v4"]).display
 
 # MAGIC %md
 # MAGIC ## Select by class with aggregations
-# MAGIC 
+# MAGIC
 # MAGIC The select output can be aggregated like a normal Spark dataframe.
 
 # COMMAND ----------
@@ -121,13 +118,13 @@ dx.delete_by_class(from_tables="discoverx*.*.*", by_class="ip_v4", values=['0.0.
 
 # MAGIC %md
 # MAGIC ## Configuration
-# MAGIC 
+# MAGIC
 # MAGIC This section is optional, and can be used to customize the behaviour of DiscoverX
 
 # COMMAND ----------
 
 # You can change the classificaiton threshold with
-dx = DX(classification_threshold=0.95)
+dx = DX(classification_threshold=0.65)
 
 # COMMAND ----------
 
@@ -160,6 +157,79 @@ dx.display_rules()
 # COMMAND ----------
 
 dx.scan(from_tables="discoverx*.*.*", sample_size=1000)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## LLMs and zero-shot-learning
+
+# COMMAND ----------
+
+from discoverx.classifiers import ZeroShotClassifier
+
+my_classifier = ZeroShotClassifier(name="my_classifier",
+                   model_name="cross-encoder/nli-deberta-v3-xsmall",
+                   candidate_labels=[
+                      "financial data",
+                      "customer information",
+                      "personal data",
+                      "IP address",
+                      "email",
+                      "anything else"
+                   ],
+                   spark=spark)
+
+# COMMAND ----------
+
+dx.scan(from_tables="discoverx*.*.*pii*", classifier=my_classifier, sample_size=10)
+
+# COMMAND ----------
+
+display(dx.scan_result())
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT
+# MAGIC     'discoverx_sample' as table_catalog,
+# MAGIC     'sample_datasets' as table_schema,
+# MAGIC     'fake_pii_examples' as table_name,
+# MAGIC     column_name,
+# MAGIC     class_name,
+# MAGIC     (avg(score)) as score
+# MAGIC FROM
+# MAGIC (
+# MAGIC     SELECT column_name, class_result.*
+# MAGIC     FROM
+# MAGIC     (
+# MAGIC         SELECT column_name, explode(classification_result) AS class_result
+# MAGIC         FROM
+# MAGIC         (
+# MAGIC             SELECT
+# MAGIC             column_name,
+# MAGIC             my_classifier_udf(value) AS classification_result
+# MAGIC             FROM (
+# MAGIC               SELECT column_name, array_join(
+# MAGIC                 concat(
+# MAGIC                 array(concat('Column name: ', column_name), 'values:'), 
+# MAGIC                 collect_list(value)
+# MAGIC                 ), ',\n', '') AS value
+# MAGIC               FROM
+# MAGIC               (
+# MAGIC                 SELECT
+# MAGIC                     stack(3, 'ip_address', `ip_address`, 'name', `name`, 'email', `email`) AS (column_name, value)
+# MAGIC                 FROM discoverx_sample.sample_datasets.fake_pii_examples
+# MAGIC                 TABLESAMPLE (100 ROWS)
+# MAGIC               )
+# MAGIC               WHERE value IS NOT NULL
+# MAGIC               GROUP BY column_name
+# MAGIC             )
+# MAGIC         )
+# MAGIC     )
+# MAGIC )
+# MAGIC GROUP BY table_catalog, table_schema, table_name, column_name, class_name
+# MAGIC
+# MAGIC -- 
 
 # COMMAND ----------
 
