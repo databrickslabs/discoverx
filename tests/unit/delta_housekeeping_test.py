c2b028f26de52f02aa44e420244785afd5b016dd
Lorenzo Rubio <lorenzo.rubio@databricks.com>
2024-01-05 18:35:33 +0100

    cleanup

--- tests/unit/delta_housekeeping_test.py
+++ tests/unit/delta_housekeeping_test.py
@@ -49,46 +49,6 @@ def expected_pdh_housekeeping_summary(request):
     return _resolve_file_path(request, "data/delta_housekeeping/expected_pdh_housekeeping_summary.csv")
 
 
-@pytest.mark.skip()
-def test_process_describe_history_template():
-    from pyspark.sql import SparkSession
-    spark = SparkSession.builder.getOrCreate()
-
-    dh = DeltaHousekeeping(spark)
-    dd_click_sales = pd.read_csv(
-        "/Users/lorenzo.rubio/Documents/GitHub/discoverx_lorenzorubi-db/tests/unit/data/delta_housekeeping/dd_click_sales.csv")
-    dh_click_sales = pd.read_csv(
-        "/Users/lorenzo.rubio/Documents/GitHub/discoverx_lorenzorubi-db/tests/unit/data/delta_housekeeping/dh_click_sales.csv")
-    expected_pdh_click_sales = pd.read_csv(
-        "/Users/lorenzo.rubio/Documents/GitHub/discoverx_lorenzorubi-db/tests/unit/data/delta_housekeeping/expected_pdh_click_sales.csv"
-    )
-
-    describe_detail_df = spark.createDataFrame(dd_click_sales)
-    # describe_detail_df = (
-    #     describe_detail_df
-    #     .withColumn("split", F.split(F.col('name'), '\.'))
-    #     .withColumn("catalog", F.col("split").getItem(0))
-    #     .withColumn("database", F.col("split").getItem(1))
-    #     .withColumn("tableName", F.col("split").getItem(2))
-    #     .select([
-    #         F.col("catalog"),
-    #         F.col("database"),
-    #         F.col("tableName"),
-    #         F.col("numFiles").alias("number_of_files"),
-    #         F.col("sizeInBytes").alias("bytes"),
-    #     ])
-    # )
-    # describe_detail_df.toPandas().to_csv("/Users/lorenzo.rubio/Documents/GitHub/discoverx_lorenzorubi-db/tests/unit/data/delta_housekeeping/dd_housekeeping_summary.csv", index=False)
-
-    describe_history_df = spark.createDataFrame(dh_click_sales)
-    describe_history_df = describe_history_df.withColumn("operation", F.lit("NOOP"))
-
-    out = dh._process_describe_history(describe_detail_df, describe_history_df)
-
-    out.toPandas().to_csv("/Users/lorenzo.rubio/Documents/GitHub/discoverx_lorenzorubi-db/tests/unit/data/delta_housekeeping/expected_pdh_housekeeping_summary.csv", index=False)
-    assert out
-
-
 def test_process_describe_history_no_optimize(spark, dh_click_sales, dd_click_sales, expected_pdh_click_sales):
     dh = DeltaHousekeeping(spark)
     describe_detail_df = spark.createDataFrame(dd_click_sales)
