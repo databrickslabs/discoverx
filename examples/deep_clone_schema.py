b9c8af12d1ba4e530ffe3e36df088adf536ccd93
Erni Durdevic <erni.durdevic@gmail.com>
2024-01-15 17:44:50 +0400

    v0.0.8 (#102)
    * v0.0.8
    
    * Updated deep clone notebook

--- examples/deep_clone_schema.py
+++ examples/deep_clone_schema.py
@@ -2,15 +2,20 @@
 # MAGIC %md
 # MAGIC # Deep Clone a Schema
 # MAGIC
-# MAGIC Databricks' Deep Clone functionality enables the effortless creation of a data replica with minimal coding and maintenance overhead. Using the `CLONE` command, you can efficiently generate a duplicate of an existing Delta Lake table on Databricks at a designated version. The cloning process is incremental, ensuring that only new changes since the last clone are applied to the table. 
+# MAGIC Databricks' Deep Clone functionality enables the effortless creation of a data replica with minimal coding and maintenance overhead. Using the `CLONE` command, you can efficiently generate a duplicate of an existing Delta Lake table on Databricks at a designated version. The cloning process is incremental, ensuring that only new changes since the last clone are applied to the table.
 # MAGIC
 # MAGIC
 # MAGIC Deep cloning is applied on a per-table basis, requiring a separate invocation for each table within your schema. In scenarios where automation is desirable, such as when dealing with shared schemas through Delta sharing, replicating the entire schema can be achieved using DiscoverX. This approach eliminates the need to manually inspect and modify your code each time a new table is added to the schema by the provider.
 # MAGIC
 # MAGIC This notebook serves as an example of utilizing DiscoverX to automate the replication of a schema using Delta Deep Clone.
 # MAGIC
-# MAGIC Our recommendation is to schedule this notebook as a job at the recipient side. 
+# MAGIC Our recommendation is to schedule this notebook as a job at the recipient side.
 # MAGIC
+
+# COMMAND ----------
+
+# MAGIC %pip install dbl-discoverx==0.0.8
+# MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------
 
@@ -19,7 +24,7 @@
 
 # COMMAND ----------
 
-dbutils.widgets.text("1.source_catalog", "_discoverx_deep_clone") 
+dbutils.widgets.text("1.source_catalog", "_discoverx_deep_clone")
 dbutils.widgets.text("2.destination_catalog", "_discoverx_deep_clone_replica")
 
 source_catalog = dbutils.widgets.get("1.source_catalog")
@@ -30,10 +35,6 @@ destination_catalog = dbutils.widgets.get("2.destination_catalog")
 # MAGIC %md
 # MAGIC ### Use DiscoverX for cloning
 
-# COMMAND ----------
-
-# %pip install dbl-discoverx==0.0.7
-# dbutils.library.restartPython()
 
 # COMMAND ----------
 
@@ -52,31 +53,32 @@ spark.sql(f"CREATE CATALOG IF NOT EXISTS {destination_catalog}")
 
 # COMMAND ----------
 
+
 def clone_tables(table_info):
-  
-  spark.sql(f"CREATE SCHEMA IF NOT EXISTS {destination_catalog}.{table_info.schema}")
-  try:
-    spark.sql(
-    f"""CREATE OR REPLACE TABLE 
+    spark.sql(f"CREATE SCHEMA IF NOT EXISTS {destination_catalog}.{table_info.schema}")
+    try:
+        spark.sql(
+            f"""CREATE OR REPLACE TABLE 
     {destination_catalog}.{table_info.schema}.{table_info.table} 
     CLONE {table_info.catalog}.{table_info.schema}.{table_info.table}
     """
-  )
-    result={
-        "source": f"{table_info.catalog}.{table_info.schema}.{table_info.table}",
-        "destination": f"{destination_catalog}.{table_info.schema}.{table_info.table}",
-        "success":True,
-        "info": None,
-      }
-  # Cloning Views is not supported
-  except Exception as error: 
-    result={
-        "source": f"{table_info.catalog}.{table_info.schema}.{table_info.table}",
-        "destination": f"{destination_catalog}.{table_info.schema}.{table_info.table}",
-        "success":False,
-        "info": error,
-      }
-  return result
+        )
+        result = {
+            "source": f"{table_info.catalog}.{table_info.schema}.{table_info.table}",
+            "destination": f"{destination_catalog}.{table_info.schema}.{table_info.table}",
+            "success": True,
+            "info": None,
+        }
+    # Cloning Views is not supported
+    except Exception as error:
+        result = {
+            "source": f"{table_info.catalog}.{table_info.schema}.{table_info.table}",
+            "destination": f"{destination_catalog}.{table_info.schema}.{table_info.table}",
+            "success": False,
+            "info": error,
+        }
+    return result
+
 
 # COMMAND ----------
 
@@ -86,5 +88,4 @@ def clone_tables(table_info):
 
 # COMMAND ----------
 
-res = dx.from_tables(f"{source_catalog}.*.*")\
-  .map(clone_tables)
+res = dx.from_tables(f"{source_catalog}.*.*").map(clone_tables)
